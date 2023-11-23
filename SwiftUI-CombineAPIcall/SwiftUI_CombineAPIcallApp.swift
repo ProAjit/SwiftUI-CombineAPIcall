47eae4303f2e97fb445fbf33289a3e0cb7f21167
Ajit Anant Satarkar <ProAjit@users.noreply.github.com>
2023-11-23 21:38:42 +0530

    Initial Commit

---
+++ SwiftUI-CombineAPIcall/SwiftUI_CombineAPIcallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CombineAPIcallApp.swift
+//  SwiftUI-CombineAPIcall
+//
+//  Created by Ajit Satarkar on 23/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CombineAPIcallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
